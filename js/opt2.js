4ef79c4a945456c91acb32b508e87c7c1d7ad733
Pratik Bariya <107101389+Bpratik2912@users.noreply.github.com>
2022-06-20 08:08:36 +0530

    Add files via upload

--- js/opt2.js
+++ js/opt2.js
@@ -2,27 +2,21 @@ let name = localStorage.getItem('name');
 let age = localStorage.getItem('age');
 let email = localStorage.getItem('email');
 let phone = localStorage.getItem('phone');
-
-
 let gender = localStorage.getItem('gender');
-let sunday=  localStorage.getItem('sunday',$('#sun').val());
-let monday = localStorage.getItem('monday',$('#mon').val());  
-let tuesday = localStorage.getItem('tuesday',$('#tues').val());
-let wednesday = localStorage.getItem('wednesday',$('#wed').val());  
-let thursday=  localStorage.getItem('thursday',$('#thurs').val());   
-let friday=  localStorage.getItem('friday',$('#fri').val());
-let saturday= localStorage.getItem('saturday',$('#sat').val());
+let sunday = localStorage.getItem('sunday', $('#sun').val());
+let monday = localStorage.getItem('monday', $('#mon').val());
+let tuesday = localStorage.getItem('tuesday', $('#tues').val());
+let wednesday = localStorage.getItem('wednesday', $('#wed').val());
+let thursday = localStorage.getItem('thursday', $('#thurs').val());
+let friday = localStorage.getItem('friday', $('#fri').val());
+let saturday = localStorage.getItem('saturday', $('#sat').val());
 
-
-
-                
-{$('#displayArea').append(
+{
+  $('#displayArea').append(
     "<tr><td>" + name +
     "</td><td>" + age +
     "</td><td>" + email +
     "</td><td>" + phone +
-    
-    
     "</td><td>" + gender +
     "</td><td>" + sunday +
     "</td><td>" + monday +
@@ -35,12 +29,14 @@ let saturday= localStorage.getItem('saturday',$('#sat').val());
     "<td><a href='' class='edit'>Edit</a></td></tr>");
 
 
-    $(document).on('click','.rem',function(){
-      $(this).parent().parent().remove();});
+  $(document).on('click', '.rem', function () {
+    $(this).parent().parent().remove();
+  });
 
-    $(document).on('click','.edit',function(event){
-      event.preventDefault();
-      var url=window.history.go(-1);
-      $('.edit').attribute('href',url);});
+  $(document).on('click', '.edit', function (event) {
+    event.preventDefault();
+    var url = window.history.go(-1);
+    $('.edit').attribute('href', url);
+  });
 
 }   
